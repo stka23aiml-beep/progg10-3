977d88f68c15af3e18bd182c0263367bdf2fc1b8
stka23aiml-beep <stka23aiml@cmrit.ac.in>
2026-06-23 00:18:41 +0530

    first commit

---
+++ src/main/java/com/program2/maven/com_prog10fu_jar/App.java
@@ -0,0 +1,10 @@
+package com.program2.maven.com_prog10fu_jar;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
